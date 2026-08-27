7dbd7abc86d1fb6e73d6fdae34e4694a12997f94
Annie Li <108150567+anccaz@users.noreply.github.com>
2026-08-27 12:52:37 -0500

    updated sections

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,17 +4,17 @@ import About from "./sections/About/About";
 import Sponsor from "./sections/Sponsor/Sponsor";
 import MLHBadge from "@/components/landing/MLHBadge";
 import Footer from "./sections/Footer/Footer";
-import FAQ from "./sections/FAQ/FAQ";
-// import Testimonials from "@/app/sections/Testimonials/Testimonials";
-// import Countdown from "./sections/Countdown/Countdown";
-// import Gallery from "@/components/Gallery/Gallery";
+// import FAQ from "./sections/FAQ/FAQ";
+import Testimonials from "@/app/sections/Testimonials/Testimonials";
+import Countdown from "./sections/Countdown/Countdown";
+import Gallery from "@/components/Gallery/Gallery";
 // import FilmStrip from "@/components/FilmStrip/FilmStrip";
-// import Stats from "@/components/Stats/Stats";
+import Stats from "@/components/Stats/Stats";
 import Donations from "./sections/Donations/Donations";
 // import Team from "./sections/Team/Team";
-import Schedule from "./sections/Schedule/Schedule";
-import Livestream from "./sections/Livestream/Livestream";
-import Menu from "./sections/Menu/Menu";
+// import Schedule from "./sections/Schedule/Schedule";
+// import Livestream from "./sections/Livestream/Livestream";
+// import Menu from "./sections/Menu/Menu";
 
 export default function Home() {
   return (
@@ -23,18 +23,18 @@ export default function Home() {
       <Navbar />
 	    <MLHBadge />
       <Jumbotron/>
-      <Livestream/>
-      <Schedule/>
-      <Menu />
-      {/* <Countdown/> */}
+      {/* <Livestream/> */}
+      {/* <Schedule/> */}
+      {/* <Menu /> */}
+      <Countdown/>
       <About/>
       {/* <FilmStrip/> */}
-      {/* <Gallery/> */}
-      {/* <Stats/> */}
-      {/* <Testimonials/> */}
+      <Gallery/>
+      <Stats/>
+      <Testimonials/>
       <Sponsor/>
       <Donations/>
-      <FAQ/>
+      {/* <FAQ/> */}
       {/* <Team/> */}
       <Footer/>
     </div>
